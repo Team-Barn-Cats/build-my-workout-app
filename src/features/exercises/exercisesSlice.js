4a1b6d6bc0ff07f96768e63de82119ac1efaddcf
Miranda <mira.brown1618@gmail.com>
2024-05-27 11:54:20 -0500

    Added FullBody Export to exercisesSlice

--- src/features/exercises/exercisesSlice.js
+++ src/features/exercises/exercisesSlice.js
@@ -1,2 +1,7 @@
 // This is where we take a subset of our array and make it nice for the other components
 
+import { EXERCISES } from '../../app/shared/EXERCISES';
+
+export const selectFullBody = () => {
+    return EXERCISES;
+};
