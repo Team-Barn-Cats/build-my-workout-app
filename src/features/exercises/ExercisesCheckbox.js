4f2af2a988ad882b6ee7ae19986652bf92d1f074
Katya Greimes <katyagreimes@gmail.com>
2024-06-08 18:29:05 -0700

    Hero Options Work

--- src/features/exercises/ExercisesCheckbox.js
+++ src/features/exercises/ExercisesCheckbox.js
@@ -1,13 +1,13 @@
 // This puts the exercise objects from the array into checkboxes
-
+// Keeping this for later: <ExerciseCard exercise={exercise}/>
 import { Formik, Field, Form } from "formik";
-import ExerciseCard from "../../components/ExercisesCard";
+
 
 const ExercisesCheckbox = ({ exercise }) => {
   const { id, name } = exercise;
   return (
     <div>
-        <ExerciseCard exercise={exercise}/>
+        
         <input type="checkbox" />
         <label>{name}</label>
     </div>
